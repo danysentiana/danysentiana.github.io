642f4978018b4448df3d5242d91ed1cee8744161
Dany Sentiana <danysentiana1200@gmail.com>
2023-05-02 13:59:40 +0700

    First Init

---
+++ dist/js/script.js
@@ -0,0 +1,78 @@
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem("color-theme") === "dark" || (!("color-theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
+
+// hide notification
+setTimeout(function () {
+  document.getElementById("notif").style.display = "none";
+}, 5500);
+
+window.addEventListener("load", () => {
+  const scroller = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+  });
+});
+setTimeout(() => {
+  scroll.update();
+}, 5000);
+
+// splide autoscroll
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide(".splide", {
+    type: "loop",
+    drag: "free",
+    focus: "center",
+    arrows: false,
+    pagination: false,
+    perPage: 6,
+    autoScroll: {
+      speed: 1,
+      pauseOnHover: false,
+    },
+    drag: false,
+    breakpoints: {
+      1023: {
+        perPage: 4,
+      },
+      420: {
+        perPage: 3,
+      },
+    },
+  }).mount(window.splide.Extensions);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#f3f4f6",
+        secondary: "#1f2937",
+        dark: "#0F172A",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-20deg)" },
+          "50%": { transform: "rotate(20deg)" },
+        },
+        wiggle2: {
+          "0%, 100%": { transform: "rotate(5deg)" },
+          "50%": { transform: "rotate(-5deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        wiggleSlow: "wiggle2 2s ease-in-out infinite",
+      },
+    },
+    fontFamily: {
+      mon: ['"Montserrat"', "sans-serif"],
+      osan: ['"Open Sans"', "sans-serif"],
+      pop: ['"Poppins"', "sans-serif"],
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
