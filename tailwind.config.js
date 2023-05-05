ab45bcd29e40879b152d0dda0bfc6795a00b6e6f
Dany Sentiana <danysentiana1200@gmail.com>
2023-05-05 08:41:55 +0700

    Add thanks page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["index.html"],
+  content: ["index.html", "thanks.html"],
   theme: {
     extend: {
       colors: {
