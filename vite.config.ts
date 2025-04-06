74e312a2049e74feba7de40c0268743521d496ff
Dany Sentiana <danysentiana1200@gmail.com>
2025-04-06 21:01:55 +0700

    fix: react router problem spa github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/danysentiana.github.io/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
