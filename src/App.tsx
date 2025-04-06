e4fc8419a663d6d86f6f2eb82b9b7f438c489a3c
Dany Sentiana <danysentiana1200@gmail.com>
2025-04-06 14:24:15 +0700

    add: SPA fallback on github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import useLenis from "./hooks/useLenis";
 
 function App() {
   useLenis(); // Apply smooth scrolling globally
+  const navigate = useNavigate();
 
-  return (
-    <Router>
-      <AppRoutes />
-    </Router>
-  );
+  useEffect(() => {
+    const redirectPath = new URLSearchParams(window.location.search).get("redirect");
+    if (redirectPath) {
+      navigate(redirectPath, { replace: true });
+    }
+  }, [navigate]);
+
+  return <AppRoutes />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; 
 import { ThemeProvider } from "@/context/ThemeContext";
 import App from "./App"; 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <App /> 
-    </ThemeProvider>
+    <BrowserRouter> 
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <App /> 
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
