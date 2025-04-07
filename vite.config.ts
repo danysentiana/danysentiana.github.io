bcb53e1544146a510f09ac9e93b6bf9635302967
Dany Sentiana <danysentiana1200@gmail.com>
2025-04-07 14:26:18 +0700

    update: vite base configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/danysentiana.github.io",
+  // base: "/danysentiana.github.io",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
