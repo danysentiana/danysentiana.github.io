fe04a6dd014d24c486a4e53c732253346e8b07c6
Dany Sentiana <danysentiana1200@gmail.com>
2025-04-19 21:36:35 +0700

    fix: TechStack icon Home tooltip not hovering in mobile

--- src/components/TechStackIcons.tsx
+++ src/components/TechStackIcons.tsx
@@ -13,6 +13,14 @@ import {
     TooltipTrigger,
 } from "@/components/ui/tooltip"
 
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+  } from "@/components/ui/popover";
+
+import useIsTouchDevice from "@/hooks/useIsTouchDevice";
+
 const techIcons = [
     siReact, siNodedotjs, siTailwindcss, siTypescript, siPhp, 
     siLaravel, siExpress, siFigma, siVuedotjs, siBootstrap,
@@ -21,31 +29,56 @@ const techIcons = [
 ];
 
 const TechStackIcons = () => {
+    const isTouch = useIsTouchDevice();
+    
     return (
-        <TooltipProvider>
-            <div className="mt-8 grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 gap-6">
+        <div className="mt-8 grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 gap-6">
             {techIcons.map((icon) => (
-                <Tooltip key={icon.title} >
-                    <TooltipTrigger className="flex justify-center items-center align-middle place-items-center content-center ">
-                        <svg 
-                            className="w-12 h-12 md:w-14 md:h-14"
-                            viewBox="0 0 24 24" 
-                            fill="none"
-                        >
-                        <path 
-                            d={icon.path} 
-                            fill={`#${icon.hex}`} 
-                            className="dark:fill-neutral-50" 
-                        />
-                        </svg>
-                    </TooltipTrigger>
-                    <TooltipContent>
-                        <p className="text-sm font-semibold">{icon.title}</p>
-                    </TooltipContent>
-                </Tooltip>
+                <div key={icon.title} className="flex justify-center items-center align-middle place-items-center content-center">
+                    {isTouch ? (
+                        <Popover>
+                            <PopoverTrigger>
+                                <svg 
+                                    className="w-12 h-12 md:w-14 md:h-14"
+                                    viewBox="0 0 24 24" 
+                                    fill="none"
+                                >
+                                    <path 
+                                        d={icon.path} 
+                                        fill={`#${icon.hex}`} 
+                                        className="dark:fill-neutral-50" 
+                                    />
+                                </svg>
+                            </PopoverTrigger>
+                            <PopoverContent className="max-w-[150px] text-center">
+                                <p className="text-sm font-semibold">{icon.title}</p>
+                            </PopoverContent>
+                        </Popover>
+                    ) : (
+                        <TooltipProvider>
+                            <Tooltip>
+                                <TooltipTrigger>
+                                    <svg 
+                                        className="w-12 h-12 md:w-14 md:h-14"
+                                        viewBox="0 0 24 24" 
+                                        fill="none"
+                                    >
+                                        <path 
+                                            d={icon.path} 
+                                            fill={`#${icon.hex}`} 
+                                            className="dark:fill-neutral-50" 
+                                        />
+                                    </svg>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p className="text-sm font-semibold">{icon.title}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </TooltipProvider>
+                    )}
+                </div>
             ))}
-            </div>
-        </TooltipProvider>
+        </div>
     );
 };
 
