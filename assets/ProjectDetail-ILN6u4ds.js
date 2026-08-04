54220fc41ca6b8d7b63fe4e3ea21f24560020537
Dany Sentiana <danysentiana1200@gmail.com>
2026-08-04 08:14:42 +0700

    Updates

--- assets/ProjectDetail-ILN6u4ds.js
+++ assets/ProjectDetail-ILN6u4ds.js
@@ -1,4 +1,4 @@
-import{c as p,l as w,r as d,j as e,L as x,m as t,S as b,t as f,B as g,F as j}from"./index-CUBpvuEi.js";import{p as v}from"./projects-C9R_vMvE.js";/**
+import{c as p,l as w,r as d,j as e,L as x,m as t,S as b,t as f,B as g,F as j}from"./index-BKpzEspK.js";import{p as v}from"./projects-C9R_vMvE.js";/**
  * @license lucide-react v0.483.0 - ISC
  *
  * This source code is licensed under the ISC license.
